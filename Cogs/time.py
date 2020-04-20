53f59d8ac667295166d7c50aa5776c14d4007ee6
OrionVi <octaviov1998@gmail.com>
2020-04-20 11:58:09 -0300

    bot Login setup /nAdded time.cog inital setup

---
+++ Cogs/time.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands
+
+class TimeCog(commands.Cog, name="time"):
+
+    def __index__(self,bot):
+        self.bot = bot
+
+
+def setup(bot):
+    bot.add_cog(TimeCog(bot))
+    print("TimeCog has been loaded")
--- main.py
+++ main.py
@@ -7,6 +7,31 @@
 
 
 
-if __name__ == "__main__":
-    print("Hello")
+import discord
+from discord.ext import  commands
 
+bot = commands.Bot(command_prefix="!")
+inital_extensions = ["Cogs.time"]
+
+
+
+@bot.event
+async def on_ready():
+    print(f"We have logged in as {bot.user}")
+
+
+
+if __name__ == "__main__":
+    for extension in inital_extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            print(f"Failed to load extension {extension}")
+
+
+
+f = open("TOKEN.txt","r")
+token = f.readline()
+f.close()
+
+bot.run(token)
