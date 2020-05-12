1e5d9b39cef7afa0bdc00fcfe91b2ee543dc73db
OrionVi <octaviov1998@gmail.com>
2020-05-12 18:33:02 -0300

    - Fixed typo

--- Cogs/reminder.py
+++ Cogs/reminder.py
@@ -187,8 +187,8 @@ class ReminderCog(commands.Cog, name="ReminderCog"):
                                       "1.`list : Lists all your reminders `\n"
                                       "2.`me : [description] in [time]`\n"
                                       "3.`prune : [reminder id as shown in list]` - Deletes Reminder\n"
-                                      "4.`prune_user : [user_id]` (Owner Only)"
-                                      "5. `db_rollback` (Owner Only)",
+                                      "4.`prune_user : [user_id]` (Owner Only)\n"
+                                      "5.`db_rollback` (Owner Only)",
                           colour=1741991)
 
         await ctx.send(embed=e)
