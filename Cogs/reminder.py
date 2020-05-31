c447ad7e004c256d9b72fa5bf678093543f79136
OrionVi <octaviov1998@gmail.com>
2020-05-31 10:29:24 -0300

    - Hehehehex2

--- Cogs/reminder.py
+++ Cogs/reminder.py
@@ -18,7 +18,7 @@ async def is_owner(ctx):
 def is_ori_cute_present(st: str) -> bool:
     check = ["CUTE", "ORI"]
 
-    if any(item in st.upper() for item in check) and "NOT" not in st:
+    if any(item in st.upper() for item in check) and "NOT" not in st and "ORI" in st.upper():
         return True
     else:
         return False
